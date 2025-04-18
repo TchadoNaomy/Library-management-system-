40d2a6f1f6d515cda911714dfac0f68b2363d1ea
Tchado Naomy <tchadonaomi470@gmail.com>
2025-04-18 13:26:21 +0200

    this is the aboutus page

---
+++ aboutus.php
@@ -0,0 +1,61 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>library services</title>
+    <link rel="stylesheet" href="aboutus.css">
+    
+</head>
+<body>
+    <header class="hero">
+          <h1>About Us</h1>
+          <p>A Full Library Service Since 1985</p>
+          <a class="call-btn" href="tel:+237695702400" >call(237)695-702-400</a>
+    </header>
+   <section class="about">
+    <div class="images">
+        <img src="images\assistance.jpg" alt="">
+        <img src="images\borror.jpg" alt="">
+        <img src="images\program for adult and children.jpg" alt="">
+        <img src="images\program.jpg" alt="">
+    </div >
+    <div class="text">
+        <h2>We Are A Full Service Library</h2>
+        <p>Our library existed in since 1985 located in 
+            yaounde Cameroon,was one of the largest and most significant libraries 
+            of the ancient world:it has scriptoria where scholars copy and translate text preserving
+             knowledge from ancient civilisation </h1></p>
+        <p><strong>Library remain vibrant hubs of Knowledge,culture, and community engagement.</strong>
+        it has scriptoria where scholars copy and translate text preserving knowledge from ancient civilisation</p>
+    </div>
+   </section>
+   <section class="services">
+     <h2>What We Do</h2>
+     <div class="cards">
+        <div class="residential">
+            <h3>BORROWING</h3>
+            <p>Borrowing books,documents,and media</p>
+            <a href="#">Learn More</a>
+        </div>
+            <div class="commercial">
+            <h3>Assistance Services</h3>
+            <p>gives Assistance to libreriance</p>
+            <a href="#">Learn More</a>
+            </div>
+            <div class="automotive">
+                    <h3> Program for children and adults</h3>
+                    <p>Reading,Workshop,Lectures</p>
+                    <a href="#" class="learnmore">Learn More</a>
+            </div>
+        </div>
+    </section>
+    <div class="footer">
+        <footer ><p>Knowledge Without Borders</p> <p>Serving Our Community With Knowledge,Resources, And Passion </p>
+        <a href="tel:+237695702400" class="call-btn">call(237)695-702-400</a>
+        </footer>
+    </div>   
+</body>
+</html>
